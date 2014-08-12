fabc4b519b24b95967e3d497c6568e7b015c4a2d
aluno <aluno@biomed-11783.local.inatel.br>
2014-08-12 10:35:21 -0300

    Primeiro Commit 12/08/2014

---
+++ FRDM-TFC/FLASH/Sources/TFC/TFC_LineScanCamera.d
@@ -0,0 +1,136 @@
+Sources/TFC/TFC_LineScanCamera.o: ../Sources/TFC/TFC_LineScanCamera.c \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/derivative.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/MKL25Z4.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdio.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdio \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstddef \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/null.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/size_t.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/wchar_t.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_rsize_t.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/eof.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/va_list.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/file_struc.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdio_api.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdarg.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdarg \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/stdarg.ARM.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/string.h \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstring \
+ C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/string_api.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/Derivative.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_Config.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_Types.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_arm_cm0.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_ARM_SysTick.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_BoardSupport.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_CrystalClock.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_Servo.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_Motor.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_ADC.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_LineScanCamera.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_Queue.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_UART.h \
+ C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_Terminal.h
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/derivative.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/MKL25Z4.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdio.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdio:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstddef:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/null.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/size_t.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/wchar_t.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_rsize_t.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/eof.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/va_list.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/file_struc.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdio_api.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdarg.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdarg:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/stdarg.ARM.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/string.h:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstring:
+
+C:/Freescale/CW\ MCU\ v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/string_api.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/Derivative.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_Config.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_Types.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_arm_cm0.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_ARM_SysTick.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_BoardSupport.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_CrystalClock.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_Servo.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_Motor.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_ADC.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_LineScanCamera.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_Queue.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_UART.h:
+
+C:/Documents\ and\ Settings/aluno/workspace/FRDM-TFC_R1.0/FRDM-TFC/Project_Headers/TFC\TFC_Terminal.h:
